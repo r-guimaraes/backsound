6caffa2a596bb000757be1417cca145100e042c4
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-12-03 00:49:36 -0200

    Primeiras etapas para inserir novos dados do usuário no registro

--- sistema/app/controllers/users_controller.rb
+++ sistema/app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-    def show
+    
+	def create
+		User.create user_params
+	end
 
+    def show
     end
+
+    private
+    	def user_params
+    		params.require(:user).permit(:name)
+    	end	
 end
