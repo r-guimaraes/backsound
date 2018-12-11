16da09aebff65c701f819371cfd6449462be03c6
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-12-11 00:12:41 -0200

    Novo controller para buscar artistas

--- sistema/config/routes.rb
+++ sistema/config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  resources :playlists
-  devise_for :users
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "home#index"
+
+  get 'artists/index'
+  get 'artists/show'
+
+  resources :playlists
+
+  devise_for :users
+
+  resources :users
 
   get "como_funciona", to: "home#como_funciona"
   get "sobre", to: "home#sobre"
   get "contato", to: "home#contato"
   get "planos", to: "home#planos"
+  get "/buscar_bandas", to: "artists#index"
+  get "/bandas/:id", to: "artists#show", as: "artist"
 end
