11776da1cee4dfb3a22db2e08160eefb6a70c304
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-12-12 20:26:58 -0200

    code format

--- sistema/app/controllers/application_controller.rb
+++ sistema/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :set_variables
-	before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def set_variables
         @slogan = "O melhor DJ é a galera"
     end
 
-	def configure_permitted_parameters
-	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])	    
-	 end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
