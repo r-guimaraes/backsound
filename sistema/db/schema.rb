d35e733c98d491f46909ae3d12ad33a1dbc938e1
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-12-12 21:34:02 -0200

    Updates currently used music's model

--- sistema/db/schema.rb
+++ sistema/db/schema.rb
@@ -40,9 +40,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_162410) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "playlist_id"
     t.string "uri"
-    t.index ["playlist_id"], name: "index_musics_on_playlist_id"
     t.index ["user_id"], name: "index_musics_on_user_id"
   end
 
